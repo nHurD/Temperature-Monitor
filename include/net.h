2319099fc6a548b3529399873dcb154bd229a5a1
Jonathan Steele <jrsteele@gmail.com>
2013-01-14 22:11:36 -0500

    Added files for network communication

---
+++ include/net.h
@@ -0,0 +1,15 @@
+//
+//  net.h
+//  BeagleBone
+//
+//  Created by Jonathan Steele on 1/14/13.
+//
+//
+
+#ifndef BeagleBone_net_h
+#define BeagleBone_net_h
+
+
+
+
+#endif
---
+++ src/net.c
@@ -0,0 +1,9 @@
+//
+//  net.c
+//  BeagleBone
+//
+//  Created by Jonathan Steele on 1/14/13.
+//
+//
+
+#include <stdio.h>
