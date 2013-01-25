ceeaa419c45cadef245e2b4f8db487d8e9c6a756
Jonathan Steele <jrsteele@gmail.com>
2013-01-25 14:21:34 -0500

    Added model for sqlite db

---
+++ web/lib/model/TemperatureData.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column
+from sqlalchemy.types import Integer, String, Float, DateTime
+
+
+# Table informaiton
+#    row_id INTEGER PRIMARY KEY AUTOINCREMENT
+#    time_data   DATETIME
+#    sensor_id   TEXT
+#    temperature REAL
+
+__all__ = ['TemperatureDAta']
+
+class TemperatureData ( Base ):
+    __tablename__ =  "TemperatureData"
+
+    row_id      = Column ( Integer, primary_key=True )
+    time_data   = Column ( DateTime )
+    sensor_id   = Column ( String )
+    temperature = Column ( Float )
+
+    def __init__ ( self, row_id, time_data, sensor_id, temperature ):
+        self.row_id         = row_id
+        self.time_data      = time_data
+        self.sensor_id      = sensor_id
+        self.temperature    = temperature
+
+  
