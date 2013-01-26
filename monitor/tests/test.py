46ba34dffebb6416de5338533eb708392a6a2ffe
Jonathan Steele <jrsteele@gmail.com>
2013-01-26 07:01:09 -0500

    Modified tests

--- monitor/tests/test.py
+++ monitor/tests/test.py
@@ -5,12 +5,15 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect(("192.168.1.6",9099))
 
-#client.send("LST\n")
+client.send("LST\n")
 
-#print client.recv(100)
+print client.recv(100)
 
-#for i in range(10):
+
 client.send("RTF 28-000004472e3e\n")
+print client.recv(100)
+
+client.send("RTC 28-000004472e3e\n")
 print client.recv(100)
 
 client.close()
